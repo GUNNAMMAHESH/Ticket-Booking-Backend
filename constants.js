96d426b999bf5c9fb2e02ae67f4a772f4df99fa3
GUNNAM MAHESH <gunnamm25@gmail.com>
2024-04-17 16:01:07 +0530

    project completed

---
+++ constants.js
@@ -0,0 +1,7 @@
+exports.constants={     //Different http error methods
+    VALIDATION_ERROR : 400,
+    UNAUTHORIZED:401,
+    FORBIDDEN:403,//access of resources
+    NOT_FOUND:404,
+    SERVER_ERROR:500
+}

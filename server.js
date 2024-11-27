34d007a37394846981d0d2c63256034647bc19e3
GUNNAM MAHESH <gunnamm25@gmail.com>
2024-11-27 18:17:45 +0530

    update cores

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cookieParse());
 const corsOptions = {
   origin: [
     "http://localhost:3000", 
-    "https://ticket-booking-frontend-kemxle9pt-maheshs-projects-7b91ea20.vercel.app/"
+    "https://ticket-booking-frontend-alpha.vercel.app"
     
   ],
   methods: "GET,POST,PUT,PATCH,DELETE",

51f03760bce2a57874873b9fa7ae2cf061b599c1
GUNNAM MAHESH <gunnamm25@gmail.com>
2024-12-26 17:18:08 +0530

    database conn

--- config/DBconnection.js
+++ config/DBconnection.js
@@ -1,14 +1,28 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const { MongoClient } = require('mongodb');
 
-const connectDb = async () => {
+// const connectDb = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URL);
+//     console.log("Connected to MongoDB");
+//   } catch (error) {
+//     console.error("MongoDB connection error:", error.message);
+//     process.exit(1); 
+//   }
+// };
+const client = new MongoClient(process.env.MONGO_URL)
+
+const connectDb = async ()=>{
   try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("Connected to MongoDB");
+    await client.connect()
+    const db = client.db();
+    console.log(`Database Connected: ${db.databaseName}`);
   } catch (error) {
-    console.error("MongoDB connection error:", error.message);
-    process.exit(1); 
+    console.log(error.message);
+    process.exit(1);
   }
-};
+}
+
 
 module.exports = connectDb;
